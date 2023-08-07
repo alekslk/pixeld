96d4a1b42f4be50346cb982d594150e48ad4da39
alekslk <102475550+alekslk@users.noreply.github.com>
2023-08-07 22:52:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,37 +4,22 @@ from PIL import Image
 root = Tk()
 root.withdraw()
 
-# Открываем диалоговое окно для выбора файла
-file_path = filedialog.askopenfilename(title="Выберите изображение", filetypes=[("JPEG files", "*.jpg"), ("PNG files", "*.png"), ("All files", "*.*")])
+# GUI to select an image file
+file_path = filedialog.askopenfilename(title="Choose an image file", filetypes=[("JPEG files", "*.jpg"), ("PNG files", "*.png"), ("All files", "*.*")])
 
-# Если путь к файлу был выбран, продолжаем работу
-print("stage 01")
+# If selected...
 if file_path:
-    # Открываем изображение
+    # Open the image
     img = Image.open(file_path)
     
-    print("stage 02")
-
-    # Преобразуем изображение в список пикселей
+    # Convert to pixels
     pixels = list(img.getdata())
-    
-    print("stage 03")
-
-    # Ищем все чёрные пиксели
-    black_pixels = [pixel for pixel in pixels if pixel == (0, 0, 0)]
-
-    
-    print("stage 04")
-    
-    
-    # Определяем количество пикселей
+    # Black pixels only
+    black_pixels = [pixel for pixel in pixels if pixel == (0, 0, 0)]   
+    # Total pixels
     total_pixels = len(pixels)
     
-    print("stage 05")
-
-    print(f"Число чёрных пикселей: {len(black_pixels)}")
-    print(f"Общее число пикселей: {total_pixels}")
     print(f"Perforation ration is: {len(black_pixels)/total_pixels}")
 
 else:
-    print("Файл не был выбран.")
+    print("File was not selected.")
